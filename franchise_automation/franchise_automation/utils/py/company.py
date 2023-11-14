f5b76f03b7775f96a7e65f55dbd48ec0511d2256
HR-EMP-007 <chandana@thirvusoft.in>
2023-11-14 17:39:45 +0530

    automation in company

--- franchise_automation/franchise_automation/utils/py/company.py
+++ franchise_automation/franchise_automation/utils/py/company.py
@@ -11,4 +11,154 @@ def company_property_setter():
 
     
 def company_custom_fields():
-    pass
+    custom_fields = {
+        "Company": [
+            dict(fieldname='franchise_automation', label='Franchise Automation',
+                fieldtype='Tab Break',insert_after='registration_details',depends_on='eval:!doc.__islocal'),
+            dict(fieldname='create_customer_supplier', label='➛ Create Customer & Supplier',
+                fieldtype='Button',insert_after='franchise_automation'),
+            dict(fieldname='update_item_tax_table', label='➛ Update Item Tax Table',
+                fieldtype='Button',insert_after='create_customer_supplier'),
+            dict(fieldname='update_mode_of_payment', label='➛ Update Mode of Payment',
+                fieldtype='Button',insert_after='update_item_tax_table'),
+            dict(fieldname='mode_of_payment', label='Mode of Payment',
+                fieldtype='Table',insert_after='update_mode_of_payment',options='Company Mode of Payment'),
+            dict(fieldname='create_users', label='➛ Create Users',
+                fieldtype='Button',insert_after='mode_of_payment'),
+            dict(fieldname='user_table', label='User Table',
+                fieldtype='Table',options='Company Users',insert_after='create_user'),
+            ],
+            
+    }
+    create_custom_fields(custom_fields)
+
+def validate_parent(doc):
+    if doc.company_type == 'Parent':
+        parent = frappe.get_all('Company',{'company_type':'Parent'})
+        if len(parent) > 1:
+            frappe.throw('Only one parent company should be created')
+
+
+@frappe.whitelist()
+def create_mode(doc):
+    doc = frappe.get_doc('Company',doc)
+    # validate_parent(doc)
+    if doc.parent_company:
+        list_doc = frappe.get_all("Mode of Payment Account",{'company':doc.name},pluck='name')
+        for j in list_doc:
+            frappe.delete_doc('Mode of Payment Account',j)
+            frappe.db.commit()
+        for i in doc.mode_of_payment:
+            if i.account and i.mode_of_payment and not frappe.db.exists('Mode of Payment Account',{'parent':i.mode_of_payment,'company':doc.name,'default_account':i.account}):
+                
+                child = frappe.get_doc('Mode of Payment',i.mode_of_payment)
+                child.append('accounts',{
+                    'company':doc.name,
+                    'default_account':i.account
+                })
+                child.flags.ignore_validate = True
+                child.save()
+    return 1
+
+@frappe.whitelist()
+def update_item_tax_table(doc):
+    doc = frappe.get_doc('Company',doc)
+    hsn_list = frappe.get_all("GST HSN Code",{'name':'82081000'},pluck='name')
+    for hs in hsn_list:
+        hs_doc = frappe.get_doc('GST HSN Code',hs)
+        if hs_doc.custom_tax_percentage:
+            tax_template = f'GST {int(hs_doc.custom_tax_percentage)}% - {doc.abbr}'
+            item_list = frappe.get_all('Item',{'gst_hsn_code':hs},pluck='name')
+            for item in item_list:
+                item_doc = frappe.get_doc('Item',item)
+                if not (frappe.db.exists('Item Tax',{'item_tax_template':tax_template,'tax_category':'In-State'})):
+                    item_doc.append('taxes',{
+                        'item_tax_template':tax_template,
+                        'tax_category':'In-State'
+                    })
+                if not (frappe.db.exists('Item Tax',{'item_tax_template':tax_template,'tax_category':'Out-State'})):
+                    item_doc.append('taxes',{
+                        'item_tax_template':tax_template,
+                        'tax_category':'Out-State'
+                    })
+                item_doc.save()
+
+    return 1
+
+@frappe.whitelist()
+def update_supplier(doc):
+    doc = frappe.get_doc('Company',doc)
+    parent = frappe.get_value('Company',{'company_type':'Parent'},'name')
+    supp = frappe.get_doc('Supplier',parent)
+    supp.append('companies',{
+        'company':doc.name
+    })
+    supp.save()
+
+
+    if not frappe.db.exists('Supplier Group','Franchise Supplier'):
+        new = frappe.new_doc('Supplier Group')
+        new.supplier_group_name = 'Franchise Supplier'
+        new.save()
+
+    if not frappe.db.exists('Customer Group','Franchise Customer'):
+        new = frappe.new_doc('Customer Group')
+        new.customer_group_name = 'Franchise Customer'
+        new.save()
+
+    # Create Supplier
+    if not frappe.db.exists('Supplier',doc.name):
+        new_sup = frappe.new_doc('Supplier')
+        new_sup.supplier_name = doc.name
+        new_sup.supplier_group = 'Franchise Supplier'
+        new_sup.supplier_type = 'Company'
+        new_sup.is_internal_supplier = 1
+        new_sup.represents_company = doc.name
+        new_sup.save()
+
+    # Create Customer
+    if not frappe.db.exists('Customer',{'customer_name':doc.name}):
+        new_cus = frappe.new_doc('Customer')
+        new_cus.company_type = 'Company'
+        new_cus.customer_group = 'Franchise Customer'
+        new_cus.customer_name = doc.name
+        new_cus.is_internal_customer = 1
+        new_cus.represents_company = doc.name
+        new_cus.save()
+
+    return 1
+
+@frappe.whitelist()
+def create_user(doc):
+    doc = frappe.get_doc('Company',doc)
+    for i in doc.user_table:
+        update_user(i,doc)
+    
+    return 1
+
+@frappe.whitelist()
+def update_user_doc(i,doc):
+    doc = frappe.get_doc('Company',doc)
+    i = frappe.get_doc('Company Users',i)
+    update_user(i,doc)
+
+def update_user(i,doc):
+    if not frappe.db.exists('User',{'email':i.email_id}):
+        user = frappe.new_doc("User")
+        user.email = i.email_id
+        user.username = i.user_name
+        user.first_name = i.user_name
+        user.role_profile_name = i.role_profile
+        user.enabled = i.enable
+        user.new_password = i.get_password(fieldname="pswd")
+        user.save()
+    else:
+        user = frappe.get_doc('User',{'email':i.email_id})
+        user.update({
+            'username': i.user_name,
+            'first_name': i.user_name,
+            'role_profile_name': i.role_profile,
+            'enabled': i.enable,
+            'new_password':i.get_password(fieldname="pswd")
+        })
+        user.save()
--- franchise_automation/install.py
+++ franchise_automation/install.py
@@ -1,4 +1,4 @@
 import frappe
-from franchise_automation.utils.py.company import company_customisation
+from franchise_automation.franchise_automation.utils.py.company import company_customisation
 def after_migrate():
     company_customisation()
